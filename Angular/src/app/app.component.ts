5f099f50e54b2647cf9edb4f6f939b83fe95a57e
Vlada Skorokhodova <vlada.skorokhodova@devexpress.com>
2025-06-09 17:27:25 +0400

    Add Angular

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
 import { ClickEvent } from 'devextreme/ui/button';
+import { Employee, Service } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [Service],
 })
 export class AppComponent {
   title = 'Angular';
-
-  counter = 0;
-
-  buttonText = 'Click count: 0';
-
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  employees: Employee[];
+  constructor(service: Service) {
+    this.employees = service.getEmployees();
+  }
+  getColumnChooserConfig() {
+    return {
+      enabled: true,
+      height: 340,
+      mode: 'select',
+      position: {
+        my: 'right top',
+        at: 'right bottom',
+        of: '.dx-cardview-column-chooser-button'
+      },
+      selection: {
+        selectByClick: true
+      }
+    };
   }
 }
---
+++ Angular/src/app/app.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+
+export interface Employee {
+    ID: number,
+    FullName: string,
+    Position: string,
+    Picture: string,
+    Department: string,
+    Phone: string,
+    Email: string,
+}
+
+const employees: Employee[] = [
+  {
+    ID: 1,
+    FullName: "John Heart",
+    Position: "CEO",
+    Picture: "images/employees/01.png",
+    Department: "Management",
+    Phone: "+1 (213) 555-9392",
+    Email: "jheart@dx-email.com",
+  },
+  {
+    ID: 2,
+    FullName: "Samantha Bright",
+    Position: "COO",
+    Picture: "images/employees/30.png",
+    Department: "Management",
+    Phone: "+1 (213) 555-2858",
+    Email: "samanthab@dx-email.com",
+  },
+  {
+    ID: 3,
+    FullName: "Arthur Miller",
+    Position: "CTO",
+    Picture: "images/employees/10.png",
+    Department: "Management",
+    Phone: "+1 (310) 555-8583",
+    Email: "arthurm@dx-email.com",
+  },
+  {
+    ID: 4,
+    FullName: "Robert Reagan",
+    Position: "CMO",
+    Picture: "images/employees/03.png",
+    Department: "Management",
+    Phone: "+1 (818) 555-2387",
+    Email: "robertr@dx-email.com",
+  },
+  {
+    ID: 5,
+    FullName: "Greta Sims",
+    Position: "HR Manager",
+    Picture: "images/employees/04.png",
+    Department: "Human Resources",
+    Phone: "+1 (818) 555-6546",
+    Email: "gretas@dx-email.com",
+  },
+  {
+    ID: 6,
+    FullName: "Brett Wade",
+    Position: "IT Manager",
+    Picture: "images/employees/05.png",
+    Department: "IT",
+    Phone: "+1 (626) 555-0358",
+    Email: "brettw@dx-email.com",
+  },
+];
+
+@Injectable()
+export class Service {
+  getEmployees() {
+    return employees;
+  }
+}
