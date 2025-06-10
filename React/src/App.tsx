362c9b07e29f8ed6a20534b9f026286d12c1eaf4
Vlada Skorokhodova <vlada.skorokhodova@devexpress.com>
2025-06-10 11:25:29 +0400

    Lint fixes

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,45 +1,47 @@
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import CardView, { Column, Paging, Pager, Selection, HeaderFilter, SearchPanel, ColumnChooser, Position } from 'devextreme-react/card-view';
+import CardView, { 
+  Column, Paging, Pager, Selection, HeaderFilter, SearchPanel, ColumnChooser, Position 
+} from 'devextreme-react/card-view';
 import { employees } from './data.ts';
 
 function App(): JSX.Element {
   return (
     <CardView
-    dataSource={employees}
-    width={1000}
-    keyExpr="ID"
-    allowColumnReordering={true}
-    cardsPerRow="auto"
-    cardMinWidth={200}
-  >
-    <Paging
-      pageSize={3}
-    />
-    <Pager
-      showInfo={true}
-      showNavigationButtons={true}
-      showPageSizeSelector={true}
-    />
-    <Selection mode="multiple" />
-    <HeaderFilter visible={true} />
-    <SearchPanel visible={true} />
-    <ColumnChooser
-      enabled={true}
-      height={340}
-      mode="select"
+      dataSource={employees}
+      width={1000}
+      keyExpr="ID"
+      allowColumnReordering={true}
+      cardsPerRow="auto"
+      cardMinWidth={200}
     >
-      <Position
-        my="right top"
-        at="right bottom"
-        of=".dx-cardview-column-chooser-button"
+      <Paging
+        pageSize={3}
       />
-      <Selection selectByClick={true} />
-    </ColumnChooser>
-    <Column dataField="FullName" allowHiding={false} />
-    <Column dataField="Position" />
-    <Column dataField="Email" />
-  </CardView>
+      <Pager
+        showInfo={true}
+        showNavigationButtons={true}
+        showPageSizeSelector={true}
+      />
+      <Selection mode="multiple" />
+      <HeaderFilter visible={true} />
+      <SearchPanel visible={true} />
+      <ColumnChooser
+        enabled={true}
+        height={340}
+        mode="select"
+      >
+        <Position
+          my="right top"
+          at="right bottom"
+          of=".dx-cardview-column-chooser-button"
+        />
+        <Selection selectByClick={true} />
+      </ColumnChooser>
+      <Column dataField="FullName" allowHiding={false} />
+      <Column dataField="Position" />
+      <Column dataField="Email" />
+    </CardView>
   );
 }
 
