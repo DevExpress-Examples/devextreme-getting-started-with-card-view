ff8a76efb6e424ec22df0aa7cae184dfa46c5236
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 09:54:54 +0300

    chore(angular): ng update 20 → 21

--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -1,7 +1,8 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   // eslint-disable-next-line no-console -- log bootstrap errors to help diagnose startup issues
   .catch((err) => console.error(err));
